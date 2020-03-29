617348019463b9b73a39c0df98212df5cf86b980
justmdiaz <justmdiaz@knights.ucf.edu>
2020-03-29 15:59:18 -0400

    Initial commit

---
+++ Space Shooter Project/Space Shooter Project/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float tumble;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+   
+}
